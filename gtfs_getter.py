8043339b7b8777425c2dadbea087d730bcddc15c
feu-follet <61912511+feu-follet@users.noreply.github.com>
2023-06-21 15:37:56 +0200

    Update gtfs_getter.py
    Making it more easy to change url

--- gtfs_getter.py
+++ gtfs_getter.py
@@ -3,14 +3,13 @@ from os import remove,listdir
 from zipfile import ZipFile
 
 _FOLDER = "gtfs/"
-
+_URL = 'https://eu.ftp.opendatasoft.com/sncf/gtfs/export-ter-gtfs-last.zip'
 
 def get_latest():
     for file in listdir(_FOLDER):
         remove(_FOLDER+file)
     
-    url = 'https://eu.ftp.opendatasoft.com/sncf/gtfs/export-ter-gtfs-last.zip'
-    r = get(url, allow_redirects=True)
+    r = get(_URL, allow_redirects=True)
     open(_FOLDER+'gtfs_last.zip', 'wb').write(r.content)
     
     with ZipFile(_FOLDER+"gtfs_last.zip", 'r') as zObject:
