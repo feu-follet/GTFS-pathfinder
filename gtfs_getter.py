d1bad7d6402a576a7a3ddd9aba66fd395146d6e2
feu-follet <61912511+feu-follet@users.noreply.github.com>
2023-06-21 16:02:36 +0200

    Update gtfs_getter.py
    specification of get_latest function

--- gtfs_getter.py
+++ gtfs_getter.py
@@ -6,6 +6,9 @@ _FOLDER = "gtfs/"
 _URL = 'https://eu.ftp.opendatasoft.com/sncf/gtfs/export-ter-gtfs-last.zip'
 
 def get_latest():
+    """
+    Downloads the file at the given url, unzips it and stores its content in the specified folder, erasing the eventual previous data
+    """
     for file in listdir(_FOLDER):
         remove(_FOLDER+file)
     
